4a2b66a6960112bb3ac949ae4db644d19e7558af
user4390 <153214456+user4390@users.noreply.github.com>
2025-05-11 17:00:32 +0600

    Create header.php

---
+++ partials/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CPA Network</title>
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<nav>
+  <a href="/">Home</a>
+  <a href="/publisher/">Publisher Login</a>
+  <a href="/advertiser/">Advertiser Login</a>
+</nav>
